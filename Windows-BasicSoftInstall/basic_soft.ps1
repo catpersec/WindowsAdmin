5d60e9476b5d12a141fca69c4424c26cba4d71e5
catpersec <45229683+catpersec@users.noreply.github.com>
2023-03-07 22:32:49 +0100

    Update basic_soft.ps1

--- Windows-BasicSoftInstall/basic_soft.ps1
+++ Windows-BasicSoftInstall/basic_soft.ps1
@@ -1,3 +1,7 @@
+$scriptDir = $PSScriptRoot
+$scriptName = $MyInvocation.MyCommand
+$scriptPath = "$scriptDir\$scriptName"
+
 try {
     $install = choco -v;
 	Write-Host "Chocolatey installed. Chocolatey version: " $install " . Software installation will proceed."\
@@ -5,24 +9,48 @@ try {
 catch {
     Write-Host "Chocolatey not installed. Chocolatey installation will be performed"  -ForegroundColor Yellow
 	Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-	start-process powershell
-    exit
-    .\basic_soft.ps1
+	& $scriptPath
+	exit
 }
+
 
 choco feature enable -n allowGlobalConfirmation
 
-choco install notepadplusplus.install --force
-choco install discord --force
-choco install vlc --force
-choco install Firefox --force
-choco install spotify --force
-choco install paint.net --force
-choco install adobereader --force
-choco install PDFXchangeEditor --force
-choco install 7zip.install --force
-choco install Bitwarden --force
-choco install windirstat --force
-choco install powershell-core --force
+## Text editors
+#choco install notepadplusplus.install --force
+
+## Image editors
+#choco install paint.net --force
+
+## Video players
+#choco install vlc --force
+
+## Music players
+#choco install spotify --force
+
+## Browsers
+#choco install Firefox --force
+#choco GoogleChrome --force
+
+## PDF reading
+#choco install adobereader --force
+
+## PDF editors
+#choco install PDFXchangeEditor --force
+
+## Archives
+#choco install 7zip.install --force
+
+## Chat apps
+#choco install discord --force
+
+## Security
+#choco install Bitwarden --force
+
+## Windows tools
+#choco install windirstat --force
+#choco install powershell-core --force
+
+
 #resources
 #https://www.reddit.com/r/chocolatey/comments/esqqfz/is_it_possible_to_install_multiple_packages_at/
